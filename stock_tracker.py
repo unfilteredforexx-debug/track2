e8862707385ae61a84853e44d6160b490351b5a4
unfilteredforexx-debug <unfilteredforexx@gmail.com>
2025-12-03 21:28:07 +0300

    Update stock_tracker.py

--- stock_tracker.py
+++ stock_tracker.py
@@ -15,126 +15,333 @@ markets = response_markets.json()
 print(markets)
 print("-" * 30)
 
-# --- 3. Get top gainers ---
-print("### Top Gainers (JSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/jse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (nSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/nse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (uSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/use/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (mSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/mse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (luSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/luse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (ngx) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/ngx/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (bSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/bse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
 
 
-print("### Top Gainers (gSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/gse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Top Gainers (zSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/zse/top-gainers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (zSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/zse/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (uSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/use/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (ngx) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/ngx/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (nSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/nse/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (mSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/mse/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (mSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/mse/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (luSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/luse/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
-print("### Bottom Losers (jSE) ###")
-url_gainers = "https://real-time-african-stock.p.rapidapi.com/exchange/jse/bottom-losers"
-response_gainers = requests.get(url_gainers, headers=HEADERS)
-gainers = response_gainers.json()
-print(gainers)
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+# Deleted gse and bse bottom losers
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+# --- 2. Get all available markets ---
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
+print("-" * 30)
+
+print("### Markets ###")
+url_markets = "https://real-time-african-stock.p.rapidapi.com/markets"
+response_markets = requests.get(url_markets, headers=HEADERS) 
+markets = response_markets.json()
+print(markets)
 print("-" * 30)
 
 # Deleted gse and bse bottom losers
